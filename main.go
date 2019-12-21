e8070cd138941ad7934c12688302a453bdf9da83
seaung <seaung.c0m7de@protonmail.com>
2019-12-21 21:06:10 +0800

    fix

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"Crawler-url-go/lib"
 	"fmt"
 	"os"
-	"Crawler-url-go/lib"
 )
 
 func main() {
@@ -20,67 +20,3 @@ func main() {
 		fmt.Println("[*] Found : ", link)
 	}
 }
-
-/*
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strings"
-
-	"github.com/PuerkitoBio/goquery"
-)
-
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("[+] Crawler all links in web page.")
-		fmt.Println("[+] Usage : ", os.Args[0], "<url>")
-		fmt.Println("[+] Example : ", os.Args[0], "https://www.localhost.com")
-		os.Exit(1)
-	}
-
-	targetUrl := os.Args[1]
-
-	client := &http.Client{}
-
-	request, err := http.NewRequest("GET", targetUrl, nil)
-
-	if err != nil {
-		log.Fatal("[-] Created request object error : ", err)
-	}
-
-	request.Header.Add("User-Agent", "__spider_man__")
-
-	response, err := client.Do(request)
-
-	urlsArr := []string{}
-
-	if err != nil {
-		log.Fatal("[-] load response content error : ", err)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(response.Body)
-
-	if err != nil {
-		log.Fatal("[-] Can't parser response document : ", err)
-	}
-
-	doc.Find("a").Each(func(i int, selector *goquery.Selection) {
-		href, err := selector.Attr("href")
-		if err {
-
-			if strings.HasPrefix(href, "/") || strings.HasPrefix(href, "//") || strings.HasPrefix(href, "javascript:;") {
-				fmt.Println("[*] the url is invalid.", href)
-			} else {
-				//fmt.Println("[+] Found url : ", href)
-				urlsArr = append(urlsArr, href)
-			}
-		}
-	})
-
-	for k, v := range urlsArr {
-		fmt.Println(k, v)
-	}
-}
-*/
